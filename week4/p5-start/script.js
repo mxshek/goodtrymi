33c0bd186dded1b2a2ced84c602513ef77475fdd
mxshek <ms9198@nyu.edu>
2023-10-02 21:05:03 -0400

    ShapeSystem Class

--- week4/p5-start/script.js
+++ week4/p5-start/script.js
@@ -1,36 +1,26 @@
-class Shape {
-  constructor(_x, _y, _w, _h, _speedX, _speedY){
-    this.x = _x
-    this.y = _y
-    this.w = _w
-    this.h = _h
-    this.speedX = _speedX
-    this.speedY = _speedY
-    this.color = 255
-  }
-
-  update() {
-    this.x += this.speedX
-    this.y += this.speedY
-  }
-
-  display() {
-    fill(this.color)
-    rect(this.x, this.y, this.w, this.h)
-  }
-}
 
 let myArray = ["it", "was", "a", "dark", "and", "stormy", "night"]
-
 let myRect = new Shape(200, 100, 50, 50, 3, 1)
 myRect.color = [200, 20, 200]
-
 let myOtherRect = new Shape(400, 500, 50, 50, -1, -1)
+
+
+let system = new ShapeSystem()
+
 
 function setup() {
   createCanvas(600, 600)
 
+  system.createShape()
+  system.createShape()
+  system.createShape()
+
+
+  console.log(system.shapes)
+
   myOtherRect.color = color(220, 120, 20)
+
+
 }
 
 function draw() {
@@ -41,4 +31,7 @@ function draw() {
 
   myRect.display()
   myOtherRect.display()
+
+  system.update()
+  system.display()
 }
